05b8658f83dbfa2b279040b4122f93ba3b098ee6
Giovanni AYIMAMBENWE <gioayima@gmail.com>
2025-04-17 20:17:22 +0100

    modification des cors

--- src/main/java/com/esiitech/monbondocteur/config/SecurityConfig.java
+++ src/main/java/com/esiitech/monbondocteur/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -42,7 +43,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, HttpSecurity httpSecurity) throws Exception {
         return
                 httpSecurity
-                        .cors(AbstractHttpConfigurer::disable)
+                        .cors(Customizer.withDefaults())
                         .csrf(AbstractHttpConfigurer::disable)
                         .authorizeHttpRequests(
                                 authorize -> authorize
