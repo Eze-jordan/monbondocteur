46cb2251e76818dc3fc7234ff562438a5157e35f
unknown <Eze-jordan>
2025-04-21 16:06:43 +0100

    mise a jour V15 21/04/25

--- src/main/java/com/esiitech/monbondocteur/config/WebConfig.java
+++ src/main/java/com/esiitech/monbondocteur/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.esiitech.monbondocteur.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,16 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Autorise toutes les routes de l'API
-                .allowedOrigins(
-                        "https://api-monbondocteur.esiitech-gabon.com", // Si tu accèdes à l'API depuis cette URL
-                        "https://moubengou-bodri.highticketdeveloper.com" // Frontend de Bodri
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Méthodes HTTP autorisées
-                .allowedHeaders("*") // Tous les headers autorisés
-                .allowCredentials(true) // Permet l'envoi de cookies/token (comme Authorization)
-                .exposedHeaders("Authorization"); // Expose le header Authorization dans la réponse (optionnel mais utile pour JWT)
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // autoriser toutes les routes
+                        .allowedOrigins("https://moubengou-bodri.highticketdeveloper.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // si tu utilises des cookies ou autorisation
+            }
+        };
     }
 }
