c731792265660aa2855b08d5336454277cc7f904
unknown <Eze-jordan>
2025-04-21 16:50:55 +0100

    mise a jour V15 21/04/25

--- src/main/java/com/esiitech/monbondocteur/config/WebConfig.java
+++ src/main/java/com/esiitech/monbondocteur/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.esiitech.monbondocteur.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,17 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // autoriser toutes les routes
-                        .allowedOrigins("https://moubengou-bodri.highticketdeveloper.com")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true); // si tu utilises des cookies ou autorisation
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://moubengou-bodri.highticketdeveloper.com") // Ton domaine
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .exposedHeaders("Authorization"); // Optionnel mais utile pour les headers comme Authorization
     }
 }
