47d557229c3e07873ab4b186d2f4014b4ea4cc28
EZE NDZINGA David (Jordan) <158742782+Eze-jordan@users.noreply.github.com>
2025-05-26 16:20:29 +0100

    Update RendezVousController.java

--- src/main/java/com/esiitech/monbondocteur/controller/RendezVousController.java
+++ src/main/java/com/esiitech/monbondocteur/controller/RendezVousController.java
@@ -8,7 +8,7 @@ import com.esiitech.monbondocteur.service.RendezVousService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
