acacbbd4ff448da5440e643c2e46025431fdfea2
EZE NDZINGA David (Jordan) <158742782+Eze-jordan@users.noreply.github.com>
2025-05-26 15:49:07 +0100

    Update WebConfig.java

--- src/main/java/com/esiitech/monbondocteur/config/WebConfig.java
+++ src/main/java/com/esiitech/monbondocteur/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/") // ou "/api/" si tu veux restreindre
-                .allowedOrigins("https://moubengou-bodri.highticketdeveloper.com")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
