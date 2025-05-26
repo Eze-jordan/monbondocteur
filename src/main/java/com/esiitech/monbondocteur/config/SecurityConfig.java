377749a9d66f4f9c42e9877a8bf886312ff65dcf
EZE NDZINGA David (Jordan) <158742782+Eze-jordan@users.noreply.github.com>
2025-05-26 16:16:54 +0100

    Update SecurityConfig.java

--- src/main/java/com/esiitech/monbondocteur/config/SecurityConfig.java
+++ src/main/java/com/esiitech/monbondocteur/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,7 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
