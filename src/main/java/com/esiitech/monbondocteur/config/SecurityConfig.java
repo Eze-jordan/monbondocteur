72c4006d3491959ba3497f94336b5b4fca317532
unknown <Eze-jordan>
2025-05-26 21:10:07 +0100

    MAJ

--- src/main/java/com/esiitech/monbondocteur/config/SecurityConfig.java
+++ src/main/java/com/esiitech/monbondocteur/config/SecurityConfig.java
@@ -15,6 +15,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
 @EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
@@ -39,31 +45,29 @@ public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, HttpSecurity httpSecurity) throws Exception {
-        return
-                httpSecurity
-                        .cors(Customizer.withDefaults()) // ← Active CORS ici
-
-                        .csrf(AbstractHttpConfigurer::disable)
-                        .authorizeHttpRequests(auth -> auth
-                                .requestMatchers(
-                                        "/api/users/activation",   // 👈 route publique
-                                        "/api/users/**",              // 👈 inscription publique
-                                        "/api/users/connexion",    // 👈 login public
-                                        "/swagger-ui/**", "/v3/api-docs/**", // 👈 Swagger public
-                                        "/api/appointment/**",
-                                        "/api/agenda/**"
-                                ).permitAll()
-
-
-                                .anyRequest().authenticated() // 👈 le reste sécurisé
-                        )
-                        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                        .authenticationProvider(authenticationProvider()) // 👈 ajoute ton provider ici
-                        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-                        .build();
-
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults()) // ← Active CORS ici
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/users/activation",
+                                "/api/users/**",
+                                "/api/users/connexion",
+                                "/swagger-ui/**", "/v3/api-docs/**",
+                                "/api/appointment/**",
+                                "/api/agenda/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
+
+
+
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
@@ -71,6 +75,23 @@ public class SecurityConfig {
         return  authenticationConfiguration.getAuthenticationManager();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(
+                "https://moubengou-bodri.highticketdeveloper.com",
+                "http://127.0.0.1:5501",
+                "http://localhost:5501"
+        ));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(List.of("Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 
 }
