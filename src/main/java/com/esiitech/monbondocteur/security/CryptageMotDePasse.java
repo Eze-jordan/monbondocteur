92d7c7d5304f23b13813b3d4674d08d267b289f6
unknown <Eze-jordan>
2025-06-11 23:32:05 +0100

    MAJ

--- src/main/java/com/esiitech/monbondocteur/security/CryptageMotDePasse.java
+++ src/main/java/com/esiitech/monbondocteur/security/CryptageMotDePasse.java
@@ -4,11 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 @Configuration
 public class CryptageMotDePasse {
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/esiitech/monbondocteur/security/JwtFilter.java
+++ src/main/java/com/esiitech/monbondocteur/security/JwtFilter.java
@@ -61,7 +61,6 @@ public class JwtFilter extends OncePerRequestFilter {
             SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         }
 
-        filterChain.doFilter(request, response);
 
 
     }
